91becd3e4089ae76fe00f57eadf33f18bab5a9fb
Grady Ward <gradyward@Gradys-MacBook-Pro.local>
2019-10-18 14:55:39 -0600

    Forgot Return Statement

--- helloworld.go
+++ helloworld.go
@@ -15,7 +15,7 @@ func HandleRequest(ctx context.Context, event BasicEvent) (string, error) {
     return "Empty event.Input not allowed", nil
   }
   if event.Input == "version" {
-    return "This is the second version of this lambda, uploaded via Travis CI + CD!!!"
+    return "This is the second version of this lambda, uploaded via Travis CI + CD!!!", nil
   }	
   return fmt.Sprintf("Hello World! event.Input=%s", event.Input ), nil
 }
