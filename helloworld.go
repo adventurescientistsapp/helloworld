80bdf6cf94d9d96c152e99466b09224c73a474f5
Grady Ward <gradyward@Gradys-MacBook-Pro.local>
2019-10-18 14:53:12 -0600

    End to End Test: Can we deploy via Travis???

--- helloworld.go
+++ helloworld.go
@@ -13,6 +13,9 @@ type BasicEvent struct {
 func HandleRequest(ctx context.Context, event BasicEvent) (string, error) {
   if event.Input == "" {
     return "Empty event.Input not allowed", nil
+  }
+  if event.Input == "version" {
+    return "This is the second version of this lambda, uploaded via Travis CI + CD!!!"
   }	
   return fmt.Sprintf("Hello World! event.Input=%s", event.Input ), nil
 }
