c0a9502802e4708ee7f1c00f60f378c69566b236
Grady Ward <gradyward@Gradys-MacBook-Pro.local>
2019-10-18 10:17:03 -0600

    Adds behavior to test Travis Build

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-        "fmt"
-        "context"
-        "github.com/aws/aws-lambda-go/lambda"
+  "fmt"
+  "context"
+  "github.com/aws/aws-lambda-go/lambda"
 )
 
 type BasicEvent struct {
-        Input string `json:"input"`
+  Input string `json:"input"`
 }
 
 func HandleRequest(ctx context.Context, event BasicEvent) (string, error) {
-        return fmt.Sprintf("Hello World! event.Input=%s", event.Input ), nil
+  if event.Input == "" {
+    return "Empty event.Input not allowed", nil
+  }	
+  return fmt.Sprintf("Hello World! event.Input=%s", event.Input ), nil
 }
 
 func main() {
-        lambda.Start(HandleRequest)
+  lambda.Start(HandleRequest)
 }
--- main_test.go
+++ main_test.go
@@ -2,14 +2,24 @@ package main
 
 import "testing"
 
-func TestHandleRequest(t *testing.T) {
+func TestHandlePopulatedRequest(t *testing.T) {
   var event BasicEvent 
   event.Input = "helloworld"
+  
   msg, err := HandleRequest(nil, event)
+  
   if err != nil {
     t.Errorf("Error was not nil!")
   }
   assertEqual(t, msg, "Hello World! event.Input=helloworld")
+}
+
+func TestHandleEmptyRequest(t *testing.T) {
+  var event BasicEvent
+  
+  msg, _ := HandleRequest(nil, event)
+  
+  assertEqual(t, msg, "Empty event.Input not allowed")
 }
 
 func assertEqual(t *testing.T, s1 string, s2 string) {
